31e0c4f89386008da37324e6e96385db60935513
GuillermoTD <guillermotd23@gmail.com>
2023-10-21 21:32:18 -0400

    Estilos para el componente header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
-
-
-
+import "./Header.css"
 const Header = ()=>{
     return(
-        <>
-        Header component works!
-        </>
+        <div className="Header">
+            <div className="HeaderContainer">
+                <div className="Header_Item">Home</div>
+                <div className="Header_Item">About</div>
+                <div className="Header_Item">Projects</div>
+                <div className="Header_Item">Contact</div>
+            </div>
+        </div>
     )
 }
 
